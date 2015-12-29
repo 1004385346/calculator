7e11cadb40ad80a4f84a2313042967f9ffdc0957
1004385346 <1004385346@q.com>
2015-12-29 19:40:18 +0800

    添加另一个工程
    因为要给计算器做时钟，所以我又新建了一个工程做时钟模型，先画了一个圆。

---
+++ asd/asd/asdDlg.h
@@ -0,0 +1,35 @@
+
+// asdDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CasdDlg 对话框
+class CasdDlg : public CDialogEx
+{
+// 构造
+public:
+	CasdDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+	enum { IDD = IDD_ASD_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickeddraw();
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+};
